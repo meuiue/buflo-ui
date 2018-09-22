03eac04f6c5a2c47aba9367604f4c9b592e93bbf
gbuffalo163 <gbuffalo@163.com>
2018-09-22 19:25:09 +0800

    update img

--- src/page/login/thirdlogin.vue
+++ src/page/login/thirdlogin.vue
@@ -2,20 +2,17 @@
 
 <template>
    <div class="third-container">
-     <img :src="qqCode" class="third-icon qq"  />
-     <img :src="wechatCode" class="third-icon wechat" />
+
    </div>
 </template>
 
 <script>
-import qqCode from "@/assets/img/code/qq-code.jpg";
-import wechatCode from "@/assets/img/code/wechat-code.jpg";
+
 export default {
   name: "thirdlogin",
   data() {
     return {
-      qqCode: qqCode,
-      wechatCode: wechatCode
+
     };
   },
   created() {},
